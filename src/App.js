ee1ba9ca3ca7781cb4e7029fbcbfce5820f128a8
Cristian-Alexandru Crișan <cristicrisan07@gmail.com>
2022-12-07 09:31:00 +0200

    assignment2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container} from "react-bootstrap";
 import LoginForm from "./Components/LoginForm";
-import {useState} from "react";
+import React, {useState} from "react";
 import AdminViewIndex from "./Components/AdminViews/AdminViewIndex";
 import CustomerView from "./Components/CustomerView";
 
@@ -21,6 +21,7 @@ function App() {
             <link rel="stylesheet" href={"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"}/>
             <script src={"https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"}/>
             <script src={"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"}/>
+
             <img src={logo} className="App-logo" alt="logo" />
         </header>
 
--- src/Components/CustomerView.js
+++ src/Components/CustomerView.js
@@ -1,92 +1,34 @@
 import React, {useEffect, useState} from "react";
-import {Button, ButtonToolbar, Card, Col, Row} from "react-bootstrap";
-import AddRestaurantModalWithButton from "./AddRestaurantModalWithButton";
-import AddFoodButtonWithModal from "./AddFoodButtonWithModal";
+import {Card, Col, Row, Toast} from "react-bootstrap";
+import SockJsClient from 'react-stomp';
+
+
 import MyToast from "./MyToast";
-import FoodMenu from "./FoodMenu";
-import SingleSelect from "./SingleSelect";
+
 
 const CustomerView = (props) => {
-    const [viewMenu,setViewMenu]=useState(false);
-    const [food,setFood]=useState([]);
+
+    const SOCKET_URL = 'http://localhost:8081/oeupb/stomp';
+
     const [showToast,setshowToast]=useState(false);
     const [messageForToast,setmessageForToast]=useState("");
-    const [restaurant,setRestaurant]=useState(null);
-    const [availableRestaurants,setavailableRestaurants]=useState([]);
-    const [nbavailres,setnbavailres]=useState(-1);
-    const getRestaurants = () =>{
-        fetch("http://localhost:8081/oeupb/getRestaurants")
-            .then(async response => {
-                const data = await response.json();
-                //check for response
-                if (!response.ok) {
-                    //get error message from body or default to response statusText
-                    if(data.length===0)  return Promise.reject("Could not fetch data about the restaurants.");
-                    else{
-                        return Promise.reject("Some weird error.");
-                    }
-                }
-                else{
-
-                    setnbavailres(data.length);
-                    console.log(data);
-                    data.map(el => setavailableRestaurants(availableRestaurants => [...availableRestaurants,el.name]));
-                }
 
 
-            })
-            .catch(error => {
-                console.error('There was an error!', error);
-            })
+
+    const hideToast =  () => {
+        setshowToast(false);
     }
-    useEffect(()=>{
-        if(messageForToast!==""){
-            setshowToast(true);
+
+    let onMessageReceived = (msg) => {
+        console.log(msg);
+        if(props.username=== msg.clientUsername ) {
+             setmessageForToast(msg.finalMessage)
+             setshowToast(true);
+             console.log(msg);
         }
-    },[messageForToast])
-    const getFood = () =>{
-        console.log("restaurant name: "+ restaurant.label);
-        fetch("http://localhost:8081/oeupb/getFood/" + restaurant.label)
-            .then(async response => {
-                const data = await response.json();
-                //check for response
-                if (!response.ok) {
-                    //get error message from body or default to response statusText
-                    if(data.length===1&&data[0].name==="")  {
-                        setmessageForToast("This restaurant hasn't added any dishes yet.");
-                        setViewMenu(false);
-                        return Promise.reject("Could not fetch data about the food.");
-                    }
-                    else{
-                        return Promise.reject("Some weird error.");
-                    }
-                }
-                else{
-                    setFood(data);
-                }
 
-
-            })
-            .catch(error => {
-                console.error('There was an error!', error);
-            })
     }
-    useEffect(()=>{
-        if(nbavailres===-1){
-            getRestaurants();
-        }
-    },[nbavailres])
-    useEffect(()=>{
-        console.log(restaurant);
-        if(restaurant!==null){
-            getFood();
-            }
-    },[restaurant])
-    useEffect(()=>{
-        if(food.length>0){
-            setViewMenu(true);
-        }
-    },[food])
+
     return (
         <>
             <Row>
@@ -98,26 +40,25 @@ const CustomerView = (props) => {
                 </Col>
 
                 <Col md={7} className="mb-2">
-                    {
-                        showToast&&
-                        <MyToast messageToShow={messageForToast}/>
-                    }
+
+                        <Toast show={showToast} onClose={hideToast} animation={false} delay={10000} autohide>
+                            <Toast.Header>
+                                <strong className="me-auto">Success.</strong>
+                                <small>1 sec ago</small>
+                            </Toast.Header>
+                            <Toast.Body>{messageForToast}</Toast.Body>
+                        </Toast>
+
                 </Col>
             </Row>
-            <br/>
-            <Row>
-                <Col sm={3}>
-                    <Button className="HomepageButton" disabled={true}> Select restaurant:</Button>
-                </Col>
-                <Col sm={3}>
-                    <SingleSelect parentFunction={setRestaurant} inputStrings={availableRestaurants}/>
-                </Col>
-            </Row>
-            <br/>
-                {
-                    viewMenu&&
-                    <FoodMenu foodList={food}/>
-                }
+            <SockJsClient
+                url={SOCKET_URL}
+                topics={['/topic/message']}
+                onConnect={console.log("Connected!!")}
+                onDisconnect={console.log("Disconnected!")}
+                onMessage={msg => onMessageReceived(msg)}
+                debug={false}
+            />
 
         </>
     );
